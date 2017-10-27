6196e4cfff40b1c3ae970ef822cc54651fc9d86c
EA Rashel <rashel320@gmail.com>
2017-10-27 20:00:58 +0600

    create log in and sign up VC

---
+++ EA_Instagram/LogInViewController.swift
@@ -0,0 +1,34 @@
+//
+//  LogInViewController.swift
+//  EA_Instagram
+//
+//  Created by Estique on 10/27/17.
+//  Copyright © 2017 Estique. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+    
+    
+    @IBOutlet var email: UITextField!
+
+    @IBOutlet var password: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func btnLogin(_ sender: Any) {
+        
+    }
+
+    @IBAction func btnSignup(_ sender: Any) {
+        
+    }
+
+}
---
+++ EA_Instagram/SignUpViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SignUpViewController.swift
+//  EA_Instagram
+//
+//  Created by Estique on 10/27/17.
+//  Copyright © 2017 Estique. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    
+    @IBOutlet var fullname: UITextField!
+    
+    @IBOutlet var email: UITextField!
+    
+    @IBOutlet var password: UITextField!
+    
+    @IBOutlet var confirmPAssword: UITextField!
+    
+    @IBOutlet var imageview: UIImageView!
+    
+    @IBOutlet var btnNextOutlet: UIButton!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func btnNextAction(_ sender: Any) {
+    }
+    
+    @IBAction func selectImage(_ sender: Any) {
+    }
+
+
+
+}
